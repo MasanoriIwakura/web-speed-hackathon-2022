7f52c2b34904b7fc91cf263fad2494955513bc5a
MasanoriIwakura <hagobutler@yahoo.co.jp>
2022-11-08 12:03:35 +0900

    Add compression

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -6,6 +6,7 @@ import fastifyStatic from "fastify-static";
  * @type {import('fastify').FastifyPluginCallback}
  */
 export const spaRoute = async (fastify) => {
+  fastify.register(import("fastify-compress"), { encodings: ["gzip"] });
   fastify.register(fastifyStatic, {
     root: join(__dirname, "public"),
     wildcard: false,
